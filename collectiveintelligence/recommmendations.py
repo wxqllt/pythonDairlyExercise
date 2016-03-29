1ae1fc527a8c14131161fada45fe13a043237d52
wxqllt <506601549@qq.com>
2016-03-29 19:10:31 +0800

    ~

--- collectiveintelligence/recommmendations.py
+++ collectiveintelligence/recommmendations.py
@@ -20,3 +20,30 @@ def sim_distance(prefs,person1,person2):
     #求所有差值的平方和
     sum_of_squares = sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
     return  1/(1+sqrt(sum_of_squares))
+
+#皮尔逊相关系数
+def sim_pearson(prefs,person1,person2):
+    si = {}
+    for item in prefs[person1]:
+        if item in prefs[person2]:si[item] = 1
+    #如果没有相关返回0
+    n = len(si)
+    if len(si) == 0: return  0
+
+    #对所有偏好求和
+    sum1 = sum([prefs[person1][item] for item in si])
+    sum2 = sum([prefs[person2][item] for item in si])
+
+    #求平方和
+    sumsq1 = pow([prefs[person1][item] for item in si],2)
+    sumsq2 = pow([prefs[person2][item] for item in si],2)
+
+    #求乘积和
+    pSum = sum(prefs[person1][item]*prefs[person2][item] for item in si)
+
+    #求pearson coefficent
+    num = pSum - (sum1*sum2/n)
+    den = sqrt((sumsq1-pow(sum1,2)/n)*(sumsq2-pow(sum2,2)/n))
+    if den == 0: return  0
+    r = num/den
+    return r
